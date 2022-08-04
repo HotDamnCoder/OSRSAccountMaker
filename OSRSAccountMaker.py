9df57d554e500793e512f2067d52082be4ca83a8
HotDamnCoder <marcusb2kl@gmail.com>
2022-08-04 23:37:05 +0300

    Changed email generation to enable bot verification

--- OSRSAccountMaker.py
+++ OSRSAccountMaker.py
@@ -6,25 +6,24 @@ from playwright.sync_api import sync_playwright
 from playwright.sync_api import Page
 
 #* CONSTANTS
+#***********************************************************************************************
 TXT_PATH = 'C:\\Users\\Marcus\\Desktop\\bots.txt'
 FAKE = Faker()
 
-TEMP_MAIL_URL = 'https://generator.email/'
+TEMP_EMAIL_URL = 'https://generator.email/'
 OSRS_REGISTER_URl = 'https://secure.runescape.com/m=account-creation/create_account?theme=oldschool'
 
 def check_for_captcha(page: Page):
     if page.locator("id=cf-challenge-stage").is_visible():
         input('Captcha found! Press enter when solved...')
 
+def goto_and_wait(page: Page, url: str):
+    page.goto(url)
+    page.wait_for_load_state('networkidle');
 
-#* GENERATE EMAIL
-mail_response = requests.get(TEMP_MAIL_URL)
-soup = BeautifulSoup(mail_response.text, features='html.parser')
 
-
-email_username = soup.find('input', {'id': 'userName'})['value']
-email_domain = soup.find('input', {'id': 'domainName2'})['value']
-email = f'{email_username}@{email_domain}'
+#* FAKE DATA GENERATION
+#**********************************************************************************************
 
 #* GENERATE USERNAME
 username = FAKE.user_name()
@@ -36,46 +35,46 @@ password = FAKE.password(length=12, special_chars=False)
 birthdate = FAKE.date(pattern='%d-%m-%Y')
 day, month, year = birthdate.split('-')
 
-#* DISPLAY VALUES:
-print('            FAKE OSRS ACCOUNT INFO           ')
-print('*********************************************')
-print(f'Email: {email}')
-print(f'Username: {username}')
-print(f'Password: {password}')
-print(f'Birthdate: {birthdate}')
-print('*********************************************')
-
-
-#* REGISTER
 with sync_playwright() as playwright:
     firefox = playwright.firefox.launch(headless=False)
-    page = firefox.new_page()
+    email_page = firefox.new_page()
 
-    page.goto(OSRS_REGISTER_URl)
-    page.wait_for_load_state('networkidle');
+    #* GENERATE EMAIL 
+    goto_and_wait(email_page, TEMP_EMAIL_URL)
+
+    email_username = email_page.locator('id=userName').input_value()
+    email_domain = email_page.locator('id=domainName2').input_value()
+    email = f'{email_username}@{email_domain}'
+
+
+    #* REGISTRATION
+    #******************************************************************************************
+
+    register_page = firefox.new_page()
+    goto_and_wait(register_page, OSRS_REGISTER_URl)
 
     #* CHECK IF REACHABLE 
-    if page.locator("text=The web server is not reachable").is_visible():
+    if register_page.locator("text=The web server is not reachable").is_visible():
         print('OSRS server not available right now. Exiting...')
         exit()
 
     #* CAPTCHA
-    check_for_captcha(page)
+    check_for_captcha(register_page)
 
     #* ACCEPT COOKIES
-    cookie_accept = page.locator("id=CybotCookiebotDialogBodyButtonDecline")
+    cookie_accept = register_page.locator("id=CybotCookiebotDialogBodyButtonDecline")
     cookie_accept.click()
 
     #* GET INPUTS
-    email_input = page.locator('id=create-email')
-    password_input = page.locator('id=create-password')
+    email_input = register_page.locator('id=create-email')
+    password_input = register_page.locator('id=create-password')
 
-    day_input = page.locator('[name=day]')
-    month_input = page.locator('[name=month]')
-    year_input = page.locator('[name=year]')
+    day_input = register_page.locator('[name=day]')
+    month_input = register_page.locator('[name=month]')
+    year_input = register_page.locator('[name=year]')
 
-    terms_button = page.locator('[name=agree_terms]')
-    submit_button = page.locator('id=create-submit')
+    terms_button = register_page.locator('[name=agree_terms]')
+    submit_button = register_page.locator('id=create-submit')
 
     #* FILL INPUTS
     email_input.fill(email)
@@ -88,16 +87,29 @@ with sync_playwright() as playwright:
     terms_button.check()
     submit_button.click()
 
-    page.wait_for_load_state('networkidle');
+    register_page.wait_for_load_state('networkidle');
 
     #* CAPTCHA
-    check_for_captcha(page)
+    check_for_captcha(register_page)
 
     time.sleep(10)
 
     firefox.close()
 
-#* WRITE VALUES TO TXT
+
+#* BOT INFO DISPLAYING AND SAVING 
+#**************************************************************************************************
+
+#* DISPLAY BOT INFO
+print('            FAKE OSRS ACCOUNT INFO           ')
+print('*********************************************')
+print(f'Email: {email}')
+print(f'Username: {username}')
+print(f'Password: {password}')
+print(f'Birthdate: {birthdate}')
+print('*********************************************')
+
+#* WRITE BOT INFO TO TXT
 with open(TXT_PATH, 'a', encoding='UTF-8') as txt_file:
     bot_type_padding = ' ' * 20
     username_padding = ' ' * (20 - len(username))
