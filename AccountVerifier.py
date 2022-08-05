f3cb2dcd57b558cd0396b0cde3fc4100f4d5344a
HotDamnCoder <marcusb2kl@gmail.com>
2022-08-05 14:19:19 +0300

    Added bots text file updating

--- AccountVerifier.py
+++ AccountVerifier.py
@@ -8,6 +8,8 @@ def check_for_captcha(page: Page):
     if page.locator("id=cf-challenge-stage").is_visible():
         input('Captcha found! Press enter when solved...')
 
+updated_file_lines = []
+
 with sync_playwright() as playwright:
     TEMP_EMAIL_URL = 'https://generator.email/'
     firefox = playwright.firefox.launch(headless=False)
@@ -15,10 +17,10 @@ with sync_playwright() as playwright:
 
     TXT_PATH = 'C:\\Users\\Marcus\\Desktop\\bots.txt'
     with open(TXT_PATH, encoding='UTF-8') as bots_txt:
-        #* Remove table headers
-        bots_txt.readline()
-        bots_txt.readline()
-        bots_txt.readline()
+        #* Remove table headers from memory
+        updated_file_lines.append(bots_txt.readline())
+        updated_file_lines.append(bots_txt.readline())
+        updated_file_lines.append(bots_txt.readline())
 
         for line in bots_txt:
             type, username, email, password, verification = (arg.strip() for arg in line.split('\t' * 3))
@@ -39,6 +41,7 @@ with sync_playwright() as playwright:
                             email_page.goto(verification_url, wait_until='networkidle')
                             check_for_captcha(email_page)
                             print(f"Email verified for {email}")
+                            verification = "*"
                         else:
                             print_error("Can't find verification url from validation button!", email)
                     else:
@@ -48,5 +51,22 @@ with sync_playwright() as playwright:
             else:
                 print_error("Email already verified!", email)
 
+
+            #* UPDATE FILE LINE
+            separator = '\t' * 3
+            bot_type_padding = ' ' * 20
+            username_padding = ' ' * (20 - len(username))
+            email_padding = ' ' * (30 - len(email))
+
+            if verification == '':
+                verification = ' '
+
+            new_line =  f'{bot_type_padding}{separator}{username}{username_padding}{separator}{email}{email_padding}{separator}{password}{separator}{verification}\n'
+            updated_file_lines.append(new_line)
+
     email_page.close()
     firefox.close()
+
+#* UPDATE FILE
+with open(TXT_PATH, mode="w", encoding='UTF-8') as bots_txt:
+        bots_txt.writelines(updated_file_lines)
