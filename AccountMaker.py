b6320528865c823d4ee37a00d70207e4ef75f359
HotDamnCoder <marcusb2kl@gmail.com>
2022-08-06 16:52:49 +0300

    Added checking for success in account making

--- AccountMaker.py
+++ AccountMaker.py
@@ -1,11 +1,13 @@
 from faker import Faker
 from playwright.sync_api import sync_playwright
 from playwright.sync_api import Page
+from datetime import date, datetime 
 
 def check_for_captcha(page: Page):
+    page.wait_for_load_state('networkidle');
     if page.locator("id=cf-challenge-stage").is_visible():
         input('Captcha found! Press enter when solved...')
-
+        page.wait_for_load_state('networkidle');
 
 #* FAKE DATA GENERATION
 #**********************************************************************************************
@@ -18,14 +20,15 @@ username = FAKE.user_name()
 #* GENERATE PASSWORD
 password = FAKE.password(length=12, special_chars=False)
 
+
 #* GENERATE DATE
-birthdate = FAKE.date(pattern='%d-%m-%Y')
+today = date.today()
+birthdate = FAKE.date(pattern='%d-%m-%Y', end_datetime= date(today.year - 18, today.month, today.day))
 day, month, year = birthdate.split('-')
 
 with sync_playwright() as playwright:
     firefox = playwright.firefox.launch(headless=False)
     email_page = firefox.new_page()
-
     #* GENERATE EMAIL 
     TEMP_EMAIL_URL = 'https://generator.email/'
     email_page.goto(TEMP_EMAIL_URL, wait_until='networkidle')
@@ -52,6 +55,7 @@ with sync_playwright() as playwright:
     #* CAPTCHA
     check_for_captcha(register_page)
 
+
     #* ACCEPT COOKIES
     cookie_accept = register_page.locator("id=CybotCookiebotDialogBodyButtonDecline")
     cookie_accept.click()
@@ -77,12 +81,21 @@ with sync_playwright() as playwright:
 
     terms_button.check()
     submit_button.click()
-
     register_page.wait_for_load_state('networkidle');
 
     #* CAPTCHA
     check_for_captcha(register_page)
 
+    #* CHECKING FOR SUCCESS
+    register_page.reload() #! THIS IS NEEDED BECAUSE OF THE URL CHANGE I THINK
+    if register_page.locator('text="You can now begin your adventure with your new account."').is_visible():
+        account_made = True
+        print("Account making successful!")
+    else:
+        account_made = False
+        print("Account making unsuccessful!")
+
+    register_page.close()
     firefox.close()
 
 
@@ -99,10 +112,11 @@ print(f'Birthdate: {birthdate}')
 print('*********************************************')
 
 #* WRITE BOT INFO TO TXT
-TXT_PATH = 'C:\\Users\\Marcus\\Desktop\\bots.txt'
-with open(TXT_PATH, 'a', encoding='UTF-8') as txt_file:
-    separator = '\t' * 3
-    bot_type_padding = ' ' * 20
-    username_padding = ' ' * (20 - len(username))
-    email_padding = ' ' * (30 - len(email))
-    txt_file.write(f'{bot_type_padding}{separator}{username}{username_padding}{separator}{email}{email_padding}{separator}{password}{separator} \n')
+if account_made:
+    TXT_PATH = 'C:\\Users\\Marcus\\Desktop\\bots.txt'
+    with open(TXT_PATH, 'a', encoding='UTF-8') as txt_file:
+        separator = '\t' * 3
+        bot_type_padding = ' ' * 20
+        username_padding = ' ' * (20 - len(username))
+        email_padding = ' ' * (30 - len(email))
+        txt_file.write(f'{bot_type_padding}{separator}{username}{username_padding}{separator}{email}{email_padding}{separator}{password}{separator} \n')
